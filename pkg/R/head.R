81a9c023d7325f72d76c10751615f999da1ac0e3
Colin Gillespie <csgillespie@gmail.com>
2015-09-21 13:05:31 +0100

    Making way for version 2

--- pkg/R/head.R
+++
@@ -1,5 +0,0 @@
-# # #' @export
-# # head.plmcmc_mat = function (x, n = 6L, ...) head.matrix(x, n=n, ...)
-# # 
-# #' @export
-# tail.plmcmc_mat = function (x, n = 6L, ...) tail.matrix(x, n=n, ...)
--- pkg/R/subsetting.R
+++
@@ -1,4 +0,0 @@
-#' @export
-`[.plmcmc_mat`  = function(x, i, j, ... , drop = TRUE) {
-  structure(NextMethod('['), class=class(x), kernel=attr(x, "kern"))
-}
